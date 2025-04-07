4bb83decf231158e3f53f49d49b9a3418ef75351
DANILO DE BRITO RODRIGUES <danilo.brodrigues@a.ucb.br>
2025-04-07 20:56:32 -0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+
+import { createServer } from 'node:http';
+import express from "express"
+
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+server.listen(port, 'localhost', () => {
+  console.log('Listening on localhost:3000');
+});
+
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="header-circle"></div>
+       <h1>TASKFLOW</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
