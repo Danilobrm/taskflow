343dbe2644e791ba110d4e2331317670b027ad82
David <91037221+DavidGSJR@users.noreply.github.com>
2025-06-20 23:03:25 -0300

    Delete add_priority_column.sql

--- add_priority_column.sql
+++
@@ -1,8 +0,0 @@
--- Adicionar coluna priority na tabela tasks
--- Execute este SQL no SQL Editor do Supabase
-
-ALTER TABLE tasks 
-ADD COLUMN priority VARCHAR(10) CHECK (priority IN ('baixa', 'media', 'alta'));
-
--- Comentário sobre a coluna
-COMMENT ON COLUMN tasks.priority IS 'Prioridade da tarefa: baixa, media, alta'; 
