db8279351b014a1b22965ff7e765e64ed7c107cc
danilobrm <danilobr.web@gmail.com>
2025-06-22 13:03:37 -0300

    toast com status de autenticação

--- frontend/src/telas/conta/CriarConta.js
+++ frontend/src/telas/conta/CriarConta.js
@@ -1,94 +1,108 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function CriarConta() {
-    const [nome, setNome] = useState("");
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [mensagem, setMensagem] = useState("");
-    const navigate = useNavigate();
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setMensagem("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await fetch("http://localhost:3001/api/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name: nome, email, password: senha }),
-            });
+    let errors = [];
 
-            const data = await response.json();
+    if (!nome) {
+      errors.push("Nome é obrigatório.");
+    }
 
-            if (response.ok) {
-                setMensagem(data.message);
-                navigate("/login"); // Redireciona para a página de login após o registro
-            } else {
-                setMensagem(data.message || "Erro ao registrar usuário.");
-            }
-        } catch (error) {
-            console.error("Erro:", error);
-            setMensagem("Erro ao conectar ao servidor.");
-        }
-    };
+    if (!email) {
+      errors.push("Email é obrigatório.");
+    }
 
-    return (
-        <div className="auth-container">
-            <div className="auth-left-panel">
-                <h1>Criar conta</h1>
-                <p>Crie sua conta e acesse suas tarefas de qualquer lugar</p>
+    if (!senha) {
+      errors.push("Senha é obrigatória.");
+    }
+
+    if (errors.length > 0) {
+      errors.forEach((error) => toast.error(error));
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3001/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: nome, email, password: senha }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        toast.error(data.error || "Erro ao registrar usuário.");
+        return;
+      }
+      console.log(data);
+      toast.success(data.message || "Usuário registrado com sucesso!");
+      navigate("/login"); // Redireciona para a página de login após o registro
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-left-panel">
+        <h1>Criar conta</h1>
+        <p>Crie sua conta e acesse suas tarefas de qualquer lugar</p>
+      </div>
+      <div className="auth-right-panel">
+        <div className="auth-form-container">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="nome">Nome</label>
+              <input
+                type="text"
+                id="nome"
+                placeholder="Digite seu nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
             </div>
-            <div className="auth-right-panel">
-                <div className="auth-form-container">
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="nome">Nome</label>
-                            <input
-                                type="text"
-                                id="nome"
-                                placeholder="Digite seu nome"
-                                value={nome}
-                                onChange={(e) => setNome(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                placeholder="Digite seu email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="senha">Senha</label>
-                            <input
-                                type="password"
-                                id="senha"
-                                placeholder="Digite sua senha"
-                                value={senha}
-                                onChange={(e) => setSenha(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <button type="submit" className="auth-button">
-                            Criar conta
-                        </button>
-                        {mensagem && <p className="mensagem">{mensagem}</p>}
-                    </form>
-                    <p className="auth-links">
-                        Já possui uma conta? <a href="/login">Faça Login</a>
-                    </p>
-                </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Digite seu email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
+            <div className="form-group">
+              <label htmlFor="senha">Senha</label>
+              <input
+                type="password"
+                id="senha"
+                placeholder="Digite sua senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="auth-button">
+              Criar conta
+            </button>
+          </form>
+          <p className="auth-links">
+            Já possui uma conta? <a href="/login">Faça Login</a>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CriarConta;
--- frontend/src/telas/conta/FazerLogin.js
+++ frontend/src/telas/conta/FazerLogin.js
@@ -1,84 +1,93 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function FazerLogin({ setIsAuthenticated }) {
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [mensagem, setMensagem] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setMensagem("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await fetch("http://localhost:3001/api/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email, password: senha }),
-            });
+    let errors = [];
+    if (!email) {
+      errors.push("Email é obrigatório.");
+    }
 
-            const data = await response.json();
+    if (!senha) {
+      errors.push("Senha é obrigatória.");
+    }
 
-            if (response.ok) {
-                localStorage.setItem("token", data.token);
-                setIsAuthenticated(true);
-                setMensagem(data.message);
-                navigate("/home"); // Redireciona para a página inicial após o login
-            } else {
-                setMensagem(data.message || "Erro ao fazer login.");
-            }
-        } catch (error) {
-            console.error("Erro:", error);
-            setMensagem("Erro ao conectar ao servidor.");
-        }
-    };
+    if (errors.length > 0) {
+      errors.forEach((error) => toast.error(error));
+      return;
+    }
 
-    return (
-        <div className="auth-container">
-            <div className="auth-left-panel">
-                <h1>Acessar minha conta</h1>
+    try {
+      const response = await fetch("http://localhost:3001/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password: senha }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        toast.error(data.error || "Erro ao fazer login");
+        return;
+      }
+
+      localStorage.setItem("token", data.token);
+      setIsAuthenticated(true);
+      navigate("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-left-panel">
+        <h1>Acessar minha conta</h1>
+      </div>
+      <div className="auth-right-panel">
+        <div className="auth-form-container">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Digite seu email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <div className="auth-right-panel">
-                <div className="auth-form-container">
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                placeholder="Digite seu email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="senha">Senha</label>
-                            <input
-                                type="password"
-                                id="senha"
-                                placeholder="Digite sua senha"
-                                value={senha}
-                                onChange={(e) => setSenha(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <button type="submit" className="auth-button">
-                            Entrar
-                        </button>
-                        {mensagem && <p className="mensagem">{mensagem}</p>}
-                    </form>
-                    <div className="auth-links">
-                        <a href="/register">Crie sua conta</a>
-                        {/* <a href="/recuperar-senha">Recuperar senha</a> */}
-                    </div>
-                </div>
+            <div className="form-group">
+              <label htmlFor="senha">Senha</label>
+              <input
+                type="password"
+                id="senha"
+                placeholder="Digite sua senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+              />
             </div>
+            <button type="submit" className="auth-button">
+              Entrar
+            </button>
+          </form>
+          <div className="auth-links">
+            <a href="/register">Crie sua conta</a>
+            {/* <a href="/recuperar-senha">Recuperar senha</a> */}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default FazerLogin;
